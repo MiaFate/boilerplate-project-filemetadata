38a4565fbd2ff657cce4ef082c78c6d69f26908a
miafate <miapurplefroot@gmail.com>
2024-01-18 00:07:15 -0300

    init code

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ require('dotenv').config()
 
 var app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", function(req, res) {
+  try {
+    const { upfile } = req.body
+    res.json({ upfile: upfile })
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+})
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
