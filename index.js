889f0bcd6347283eef85ff21a4f9bf339caa7281
miafate <miapurplefroot@gmail.com>
2024-01-18 05:28:25 -0300

    added multer, first attempt to pass tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer')
 var app = express();
 
 const bodyParser = require('body-parser')
@@ -9,14 +9,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.memoryStorage()
+const upload = multer(({ storage: storage }))
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse", function(req, res) {
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
   try {
-    const { upfile } = req.body
-    res.json({ upfile: upfile })
+    const { originalname, mimetype, size } = req.file
+    res.json({ name: originalname, type: mimetype, size })
   } catch (error) {
     res.json({ error: error.message })
   }
